cb9cf18785deb2b5f9c492a213d0ce531e0e5fe2
oksuzd <okssuzdal@gmail.com>
2024-04-04 13:28:05 -0300

    refactoring, docs folder added

--- src/app/interceptors/error/error.interceptor.ts
+++ src/app/interceptors/error/error.interceptor.ts
@@ -3,7 +3,7 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { MatDialog } from "@angular/material/dialog";
@@ -17,38 +17,45 @@ export class ErrorInterceptor implements HttpInterceptor {
   constructor(
     public dialog: MatDialog,
     private weatherStoreService: WeatherStoreService
-  ) {}
+  ) {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
-      catchError(err => {
-        if (err.ok === false) {
-          if (err.error.target['requestMethod'] === 'GET') {
-            this.dialog.open(
-              ErrorWindowComponent,
-              {
-                width: '350px',
-                disableClose: true,
-                data: {
-                  error: 'region',
-                  message: 'The application does not work in some regions due to the policies of third-party API services.',
-                  location: `${this.weatherStoreService.getLocationState().city}, 
-                  ${this.weatherStoreService.getLocationState().country}`
-                }
-              });
-          } else {
-            this.dialog.open(
-              ErrorWindowComponent,
-              {
-                width: '350px', data: {
-                  error: 'extensions',
-                  message: 'The application does not work correctly due to some browser extensions (Adblock, Ghostery, etc.).',
-                }
-              });
-          }
-        }
-        return throwError(() => err);
-      })
+      catchError(err => this.defineError(err))
     );
+  }
+
+  defineError(err: HttpErrorResponse): Observable<never> {
+    console.log('err', err.url);
+    console.log('location', this.weatherStoreService.getLocationState());
+
+    if (!err.ok) {
+      if (err.url?.includes('pixabay') && this.weatherStoreService.getLocationState().id) {
+        this.dialog.open(
+          ErrorWindowComponent,
+          {
+            width: '350px',
+            disableClose: true,
+            data: {
+              error: 'region',
+              message: 'The application does not work in some regions due to the policies of third-party API services.',
+              location: `${this.weatherStoreService.getLocationState().city}, 
+                  ${this.weatherStoreService.getLocationState().country}`
+            }
+          });
+      } else {
+        this.dialog.open(
+          ErrorWindowComponent,
+          {
+            width: '350px', data: {
+              error: 'extensions',
+              message: 'The application is not working correctly. ' +
+                'The reason may be the use of some browser extensions. (Adblock, Ghostery, etc.).',
+            }
+          });
+      }
+    }
+    return throwError(() => err);
   }
 }
